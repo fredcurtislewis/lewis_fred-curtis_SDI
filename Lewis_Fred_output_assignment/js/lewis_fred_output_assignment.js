a901ca4829cee12c59dffaf6dfff2bdc56ef5bd1
Fred-Curtis <fredlewishg@gmail.com>
2015-06-05 11:55:46 -0400

    First Run
    I am still learning, not sure I know what this “Commit to Master”
    button does?

---
+++ Lewis_Fred_output_assignment/js/lewis_fred_output_assignment.js
@@ -0,0 +1,18 @@
+// Fred-Curtis Lewis SDI Section 1 05 June 2015
+
+var myFavoritePlaceOnEarth;
+myFavoritePlaceOnEarth = "Norway It is so beautiful";
+
+var speaksFourLanguages = true;  // Assigning a Boolean value
+
+var firstLanguage = "Norwegian"; // Assigning a String value
+
+var secondLanguage = "English"; // Assigning a String value
+
+var thirdLanguage = "Korean"; // Assigning a String Value
+
+var fourthLanguage = "German"; // Assigning a String Value
+
+var countriesVisited = 33; // Assigning a Number Value
+
+
