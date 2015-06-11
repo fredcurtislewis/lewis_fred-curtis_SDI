99882607a59e9a5f2ace1ace2ad0792086961f9d
Fred-Curtis <fredlewishg@gmail.com>
2015-06-10 23:45:13 -0400

    Expressions
    First two problems

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,10 @@
+// Fred Lewis June 10 Expressions: Dog Years Problem
+
+var sparkyAge = 4 // Sparky's actual age
+var dogYears;
+dogYears = sparkyAge * 7 // Calculate Sparky's dog age by multiplying age by 7
+
+
+console.log(" Sparky is " + " " + sparkyAge + " " + " human years old which is "+ " " + dogYears + " " + " in dog years");
+
+
---
+++ Expressions_worksheet/js/sliceofpie1.js
@@ -0,0 +1,9 @@
+// Fred Lewis June 10 2015 Expressions: Slice of Pie Pt 1
+
+var pizzaSlices = 8; //How many slices of pizza on each pie
+var partyPeople = 12; // How many people showed up to the party
+var pizzaOrdered= 4;  // How many pizzas were ordered
+var eachEaten= pizzaSlices * pizzaOrdered / partyPeople ;
+
+console.log(" Each person ate" + " " + eachEaten + " slices of pizza at the party");
+
