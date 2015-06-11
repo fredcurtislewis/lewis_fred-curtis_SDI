7257425f5211a70eb4e83d53c3927405e82fcecd
Fred-Curtis <fredlewishg@gmail.com>
2015-06-10 23:59:08 -0400

    Pie 2
    Pie 2 problem

---
+++ Expressions_worksheet/js/sliceofpie2.js
@@ -0,0 +1,7 @@
+// Fred Lewis 10 June 2015 Expressions: Slice of Pie 2
+
+var studentLeftover = 0.6;
+var numberStudents= 12;
+var sparkysCut = studentLeftover * numberStudents;
+
+console.log("Sparky got" + " " + sparkysCut + " " + "slices of pizza ");
