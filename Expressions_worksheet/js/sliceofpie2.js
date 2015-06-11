08bb3b91203c93561a3a1e50654cd503d1559de8
Fred-Curtis <fredlewishg@gmail.com>
2015-06-11 00:01:47 -0400

    comments
    comments on pie 2

--- Expressions_worksheet/js/sliceofpie2.js
+++ Expressions_worksheet/js/sliceofpie2.js
@@ -1,7 +1,7 @@
 // Fred Lewis 10 June 2015 Expressions: Slice of Pie 2
 
-var studentLeftover = 0.6;
-var numberStudents= 12;
-var sparkysCut = studentLeftover * numberStudents;
+var studentLeftover = 0.6; // students had 2 slices each with 0.6 left over
+var numberStudents= 12; // There were 12 students who showed to the party
+var sparkysCut = studentLeftover * numberStudents; // Sparky got 0.6 slices from 12 people
 
-console.log("Sparky got" + " " + sparkysCut + " " + "slices of pizza ");
+console.log("Sparky got" + " " + sparkysCut + " " + "slices of pizza "); // His total cut was 7.1999999 slices
