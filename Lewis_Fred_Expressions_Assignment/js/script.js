d6b3d56763bb81b2b8fe575592167cdd5ca8e55f
Fred-Curtis <fredlewishg@gmail.com>
2015-06-11 22:59:37 -0400

    with comments
    Expressions with comments

--- Lewis_Fred_Expressions_Assignment/js/script.js
+++ Lewis_Fred_Expressions_Assignment/js/script.js
@@ -1,8 +1,8 @@
 // Fred Lewis 10 June 2015 Expressions Assignment
 
-var gardenLength = prompt("How Long is you garden in feet?");
-var gardenWidth = prompt("How wide is your garden in feet?");
-var acre = 43560;
-var gardenAcres = (gardenLength * gardenWidth) / acre;
-alert("Your garden plot is" + " " + gardenAcres + " " + "acre(s)");
-console.log(gardenAcres);
+var gardenLength = prompt("How Long is you garden in feet?"); // User inputs how long their garden is here
+var gardenWidth = prompt("How wide is your garden in feet?"); // User inputs how wide their garden is here
+var acre = 43560; // one acre is equal to 43560 square feet
+var gardenAcres = (gardenLength * gardenWidth) / acre; // multiplying the length by the width gives you the square feet, dividing it by one acre tells you the size of you plot in acres
+alert("Your garden plot is" + " " + gardenAcres + " " + "acre(s)"); // displays the answer to the size of the garden
+console.log(gardenAcres); // prints to console
