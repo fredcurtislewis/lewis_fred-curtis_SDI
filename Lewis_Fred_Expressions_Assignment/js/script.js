04971443333a88d673264b25389a33d0895b8fb2
Fred-Curtis <fredlewishg@gmail.com>
2015-06-11 22:54:05 -0400

    First run through
    no comments

--- Lewis_Fred_Expressions_Assignment/js/script.js
+++ Lewis_Fred_Expressions_Assignment/js/script.js
@@ -1,4 +1,8 @@
 // Fred Lewis 10 June 2015 Expressions Assignment
 
 var gardenLength = prompt("How Long is you garden in feet?");
-console.log(gardenLength);
+var gardenWidth = prompt("How wide is your garden in feet?");
+var acre = 43560;
+var gardenAcres = (gardenLength * gardenWidth) / acre;
+alert("Your garden plot is" + " " + gardenAcres + " " + "acre(s)");
+console.log(gardenAcres);
