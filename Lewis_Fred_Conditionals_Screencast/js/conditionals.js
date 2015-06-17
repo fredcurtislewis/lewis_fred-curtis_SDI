900cfb550278eedeee57297ae38ec6ac6753100b
Fred-Curtis <fredlewishg@gmail.com>
2015-06-17 13:38:33 -0400

    Screencast conditionals
    more

--- Lewis_Fred_Conditionals_Screencast/js/conditionals.js
+++ Lewis_Fred_Conditionals_Screencast/js/conditionals.js
@@ -2,13 +2,28 @@
 //SDI Conditionals Screecast follow-along
 // 17 June 2015
 
-var oldEnough = true;
+//var oldEnough = true;
+//var kidHeight = 50;
 
 // if the child is old enough print to console "u can ride!"
+// if the kid is over 48 inches of height
+//if (kidHeight> 48){
+//    console.log("You are tall enough")
+//}
 
-
-if (oldEnough){
+//if (oldEnough){
     // code performed if condition is true
-    console.log("You can ride the coaster!")
-}
+  //  console.log("You can ride the coaster!")
+//}
 
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck= 200;
+
+if (iPhonePrice < budget || payCheck> 300){
+    //if the iphonePrice is less than our budget
+    console.log("We can buy the phone!");
+}else{
+
+    console.log("No phone for you!");
+}
