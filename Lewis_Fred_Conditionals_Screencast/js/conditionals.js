c73d21cb74fbcabb1ffa2b3a8c192202e7e8ca77
Fred-Curtis <fredlewishg@gmail.com>
2015-06-17 13:02:32 -0400

    Conditionals Screencast
    practice

---
+++ Lewis_Fred_Conditionals_Screencast/js/conditionals.js
@@ -0,0 +1,14 @@
+// Lewis Fred
+//SDI Conditionals Screecast follow-along
+// 17 June 2015
+
+var oldEnough = true;
+
+// if the child is old enough print to console "u can ride!"
+
+
+if (oldEnough){
+    // code performed if condition is true
+    console.log("You can ride the coaster!")
+}
+
