1892ba88282bc1b8f7b65d38cff328028b744d6a
Fred-Curtis <fredlewishg@gmail.com>
2015-06-18 23:56:12 -0400

    Conditionals Assignment
    Final

--- Lewis_Fred_Conditionals_Assignment/js/conditionals.js
+++ Lewis_Fred_Conditionals_Assignment/js/conditionals.js
@@ -7,30 +7,30 @@
 var airlineTicket   = 1000;
 var hotelRoom       = 0;
 var carCost         = 0;
-var food            = 0;
+var food            = 400;
 var numberTravelers = 0;
 
 
 
 //prompts to get the info for my calculator
 
-var airlineTicket = prompt("Enter the amount your airline ticket cost here");
+var airlineTicket = prompt("Enter the amount your airline ticket cost here"); // cost of a ticket
 
-var hotelRoom         = Number(prompt("Enter the amount you will pay for a hotel room. Enter 0 if you will not need a hotel"));
-var carCost           = Number(prompt("Enter the price of the rental car. If you do not need one enter 0"));
+var hotelRoom         = Number(prompt("Enter the amount you will pay for a hotel room. Enter 0 if you will not need a hotel")); //hotel room for five days
+var carCost           = Number(prompt("Enter the price of the rental car. If you do not need one enter 0")); // car rental cost
 
-var numberTravelers   = Number(prompt("How many travelers are going total, including yourself?"));
-var ticketTotal       = numberTravelers *  airlineTicket;
-var expensesTotal     = hotelRoom + carCost;
-var tripExpense       = ticketTotal + expensesTotal;
-var totalTrip         = tripExpense + food;
+var numberTravelers   = Number(prompt("How many travelers are going total, including yourself?")); // number of airline tickets needed
+var ticketTotal       = numberTravelers *  airlineTicket; // the number of tickets multiplied by th cost of one ticket
+var expensesTotal     = hotelRoom + carCost; // hotel and car rental expenses
+var tripExpense       = ticketTotal + expensesTotal; // all expenses minus provisions
+var totalTrip         = tripExpense + food; // the whole package
 console.log("Your total cost for Hawaii is " + totalTrip + " " + "!!!!!!");
 //console.log(ticketTotal);
 //console.log(tripExpense);
 
 // Operators (Ternary and logical and if / else)
 
-(carCost > 0) ? console.log("Car rental costs" + " " + carCost + " " + "dollars!!"):console.log("No car rental costs!!!");
+(carCost > 0) ? console.log("Car rental costs" + " " + carCost + " " + "dollars!!"):console.log("No car rental costs!!!"); // ternary on the costs of the rental car
 
 if(hotelRoom > 0){
      // enter  amount for hotel for five Days in Hawaii
