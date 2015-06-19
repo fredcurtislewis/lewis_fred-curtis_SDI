230cb0d774ae736f5cc993be68e4d78facf1c83e
Fred-Curtis <fredlewishg@gmail.com>
2015-06-18 23:13:09 -0400

    Conditionals Assignment
    last attempt

--- Lewis_Fred_Conditionals_Assignment/js/conditionals.js
+++ Lewis_Fred_Conditionals_Assignment/js/conditionals.js
@@ -4,19 +4,17 @@
 
 // variables
 
-var airlineTicket   = 600;
-var hotelRoom       = 100;
+var airlineTicket   = 1000;
+var hotelRoom       = 0;
 var carCost         = 0;
-var food            = 400;
-var numberTravelers = 3;
+var food            = 0;
+var numberTravelers = 0;
 
 
 
 //prompts
 
-//var airlineTicket = prompt("Enter the amount your airline ticket cost here");
-//var hotelRoom     = prompt("Will you rent a hotel room? Enter true for yes and false for no");
-//var rentalCar     =prompt("Will you rent a car? Enter true for yes and false for no");
+var airlineTicket = prompt("Enter the amount your airline ticket cost here");
 
 var hotelRoom         = prompt("Enter the amount you will pay for a hotel room. Enter 0 if you will not need a hotel");
 var carCost           = prompt("Enter the price of the rental car. If you do not need one enter 0");
@@ -27,16 +25,18 @@ var expensesTotal     = hotelRoom + carCost;
 var tripExpense       = ticketTotal + expensesTotal;
 var totalTrip         = tripExpense + food;
 console.log("Your total cost for Hawaii is " + totalTrip + " " + "!!!!!!");
+//console.log(ticketTotal);
+//console.log(tripExpense);
 
 // Operators (Ternary and logical and if / else)
 
 (carCost > 0) ? console.log("Car rental costs" + " " + carCost + " " + "dollars!!"):console.log("No car rental costs!!!");
 
 if(hotelRoom > 0){
-    // enter  amount for hotel for five Days in Hawaii
-    console.log("Your total cost for lodging is" + " " + hotelRoom + " " + " dollars!!")
+     // enter  amount for hotel for five Days in Hawaii
+    console.log("Your total cost for lodging is" + " " + hotelRoom + " " + " dollars!!");
 }else{
-    console.log("Your total cost for lodging is zero dollars!!")
+    console.log("Your total cost for lodging is zero dollars!!");
 }
 
 
