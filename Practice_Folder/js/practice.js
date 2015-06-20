37138fcfc1a6b3664b4c68407a092b5775eb277a
Fred-Curtis <fredlewishg@gmail.com>
2015-06-19 20:07:26 -0400

    practice

---
+++ Practice_Folder/js/practice.js
@@ -0,0 +1,24 @@
+// practice makes perfect
+
+// I am going to create a box that tells Christoper to do the chores
+// variables
+
+var chickenFeed = true;
+var cowFeed     = true;
+var pigFeed     = true;
+
+// promts to gain informations
+var chickenFeed = prompt("Have the chickens been fed?");
+var cowFeed     = prompt("Have the cows been fed?");
+var pigFeed     = prompt("Have the pigs been fed?");
+
+// Ifs or else &&
+
+if(chickenFeed = true){
+    // enter if the chickens cows and pigs have been fed or not
+    console.log("Great job on the chickens, take a break");
+}else{
+    // if they have not all been fed chores are not done
+    console.log("You better finsih the chores!!!!!!!!");
+}
+alert("Great Job!!!");
