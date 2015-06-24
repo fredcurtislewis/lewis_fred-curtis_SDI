1115c9dd28097fc920994b14f899ff73d0665d5c
Fred-Curtis <fredlewishg@gmail.com>
2015-06-23 23:38:10 -0400

    Beesting
    Attempt 1

---
+++ functions_worksheet/js/beesting.js
@@ -0,0 +1,17 @@
+/* Lewis Fred
+  SDI Functions worksheet
+  23 June 2015
+  */
+// variables and prompts
+
+var deadlyStings = 8.666666667;
+var myWeight     = prompt("Please enter your weight in pounds:");
+
+
+var myDeath      = deadlyStings * myWeight; // the formula for amount of beestings it takes to kill you.
+
+// functions
+
+function stingValidation(stings){   // function attempt
+
+
