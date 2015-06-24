3aa431fc81e081245e274e1f852f289044616101
Fred-Curtis <fredlewishg@gmail.com>
2015-06-23 22:43:00 -0400

    Functions Worksheet
    first problem

--- functions_worksheet/js/functions.js
+++ functions_worksheet/js/functions.js
@@ -3,5 +3,28 @@
     23 June 2015
  */
 
+// Variables & Prompts
+
+var radius = 10;
+var pi     = 3.14;
+var myCircumference = prompt("What is the radius of the circle?");
+
+// functions
+
+function calculateCirc(circumference){
+
+    console.log("You entered" + myCircumference);
+
+
+}
+
+
+
+// main code
+calculateCirc();
+
+var circumference = calculateCirc(2 * pi *radius);
+console.log("The circumference of the cirlce is  " +circumference);
+
 
 // alert("Works!!!");
