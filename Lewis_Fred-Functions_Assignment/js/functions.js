636db1645869a8ed3c884c2564ec7f477d2ebe45
Fred-Curtis <fredlewishg@gmail.com>
2015-06-24 23:49:20 -0400

    Functions Assignment
    Set-up

---
+++ Lewis_Fred-Functions_Assignment/js/functions.js
@@ -0,0 +1,19 @@
+/* Lewis, Fred
+   SDI Functions Assignment
+   June 24 2015
+ */
+
+//variables and prompts
+
+
+
+// functions
+
+
+
+// main code
+
+
+
+
+//alert("Does it work?"); test to see if my js is working properly
