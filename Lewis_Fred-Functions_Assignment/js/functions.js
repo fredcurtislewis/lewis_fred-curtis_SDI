3fe80079f3341297149675a317bd32f6ed7fb0c7
Fred-Curtis <fredlewishg@gmail.com>
2015-06-25 00:06:36 -0400

    Functions Assignment
    Figuring out the layout

--- Lewis_Fred-Functions_Assignment/js/functions.js
+++ Lewis_Fred-Functions_Assignment/js/functions.js
@@ -3,17 +3,32 @@
    June 24 2015
  */
 
-//variables and prompts
+// Variables & Prompts
 
+var myLottery        = prompt("Enter Florida or Powerball"); // where the user inputs whether they want Florida lottery or Powerball numbers
 
 
 // functions
 
+function lotteryValidation(lotteryNumbers){   // function attempt
 
+   if(lotteryNumbers === "Florida"){
+
+      console.log("The Florida numbers are  " + lotteryNumbers + "!");
+
+
+   }
+
+
+   return lotteryNumbers; // the return gives lotteryNumbers back to myLottery
+
+}
 
 // main code
 
 
+myLottery            = lotteryValidation(myLottery); // returning the return to myLottery
+console.log("The lottery numbers are  " + myLottery);  // displays the lottery numbers choosen
 
 
 //alert("Does it work?"); test to see if my js is working properly
