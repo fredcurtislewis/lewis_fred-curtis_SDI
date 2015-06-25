16e4d44c7ab47583824a39b85ad4e3c9717e2a7d
Fred-Curtis <fredlewishg@gmail.com>
2015-06-25 02:27:20 -0400

    Functions Assignment
    fully functional without all comments and clean up performed

--- Lewis_Fred-Functions_Assignment/js/functions.js
+++ Lewis_Fred-Functions_Assignment/js/functions.js
@@ -5,54 +5,63 @@
 
 // Variables & Prompts
 
-//var myLottery        = prompt("Enter Florida or Powerball"); // where the user inputs whether they want Florida lottery or Powerball numbers
+var myLottery        = prompt("Enter Florida or Powerball"); // where the user inputs whether they want Florida lottery or Powerball numbers
 var floridaNum;
+var powerBall;
+var thePowerBall
 
 // functions
 
-//function lotteryValidation(lotteryNumbers){   // function attempt
 
+if (myLottery === "Florida") {
 
+    function floridaLotto(min, max, num) {
+        var lotteryArray = [];
 
-   //if(lotteryNumbers === "Florida"){
-
-function floridaLotto (min, max, num) {
-
-    var lotteryArray     = [];
-
-    for (var f = 0; f < num; f++) {
-        var floridaLottery = Math.random() * (max - min) + min;
-        lotteryArray [f] = Math.round(floridaLottery);
-        //console.log("The Florida numbers are  " + floridaLottery + "!");
+        for (var f = 0; f < num; f++) {
+            var floridaLottery = Math.random() * (max - min) + min;
+            lotteryArray [f] = Math.round(floridaLottery);
+            //console.log("The Florida numbers are  " + floridaLottery + "!");
+        }
+        return lotteryArray;
     }
-    return lotteryArray;
+}else{
 
+    function powerBallNum(min, max, num) {
+        var lotteryArray = [];
 
+        for (var f = 0; f < num; f++) {
+            var floridaLottery = Math.random() * (max - min) + min;
+            lotteryArray [f] = Math.round(floridaLottery);
+            //console.log("The Florida numbers are  " + floridaLottery + "!");
+        }
+        return lotteryArray;
+    }
+    function thePowerBallNum(min, max, num) {
+        var powerArray = [];
 
+        for (var p = 0; p < num; p++) {
+            var powerBallLottery = Math.random() * (max - min) + min;
+            powerArray [p] = Math.round(powerBallLottery);
+            //console.log("The Florida numbers are  " + floridaLottery + "!");
+        }
+        return powerArray;
+    }
 
-           //}
-
-
-
-   //}else{
-      // var powerBall        = Math.random() * (42-1)+1;
-    //   powerBall            = Math.round(powerBall);
-    //   console.log("The numbers are " + powerBall + " and the Powerball is " + "!");
-
-   //}
-
-
-   //return lotteryNumbers; // the return gives lotteryNumbers back to myLottery
 
 }
 
 // main code
 
-floridaNum           = floridaLotto(1, 53, 6);
-console.log("Your Florida Lottery numbers are " + floridaNum + "!");
+if(myLottery === "Florida") {
+    floridaNum = floridaLotto(1, 53, 6);
+    console.log("Your Florida Lottery numbers are " + floridaNum + "!");
+}else{
+    powerBall            = powerBallNum(1, 59, 5);
+    thePowerBall         = thePowerBallNum(1, 35, 1);
+    console.log("Your numbers are "+ powerBall + " and your power ball is " + thePowerBall + "!");
+}
 
-// myLottery            = lotteryValidation(myLottery);// returning the return to myLottery
-// console.log("The lottery numbers are  " + myLottery);  // displays the lottery numbers choosen
 
 
 //alert("Does it work?"); test to see if my js is working properly
